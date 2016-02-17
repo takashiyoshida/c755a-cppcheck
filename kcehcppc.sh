6d1de95efb15123df43692e9f9468f9ebe3b1386
Takashi Yoshida <takashi@takashiyoshida.org>
2016-02-17 22:38:07 +0800

    Add documentation in kcehcppc.sh

--- kcehcppc.sh
+++ kcehcppc.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 
+# Print ever useful ha(e)lp message
 print_halp()
 {
-    echo "Usage: $0 -l LEVEL [-x] [files or paths]"
-    echo "-l, --level"
-    echo "-x, --xml"
+    echo "Usage: `basename $0` -l LEVEL [-x] [files or paths]"
+    echo "  -l, --level       Equivalent to --enable in cppcheck"
+    echo "                    all, warning, style, performance, portability, information,"
+    echo "                    unusedFunction, missingInclude"
+    echo "  -x, --xml         When specified, equivalent to --xml-version=2 in cppcheck"
+    echo "  [files or paths]  A list of source files or directories containing source files"
+    echo "Example usage:"
+    echo "  `basename $0` -l warning -x 2> cppcheck.xml"
 }
 
+# If no parameters are provided, print the ha(e)lp message and exit
 if [ $# -eq 0 ]; then
     print_halp
     exit 0
 fi
 
+# CPPCHECK_LEVEL: Equivalent to --enable option in `cppcheck`
+# It must be one of the following: all, warning, style, performance, portability,
+#                                  information, unusedFunction, missingInclude
+# This parameter is required
 unset CPPCHECK_LEVEL
+
+# OUTPUT_FORMAT: When specified, it is equivalent to --xml-version option
+#                in `cppcheck`
+# This parameter is not required
 OUTPUT_FORMAT=""
+
+# SOURCES: A list of source files or paths containing source files
 unset SOURCES
 
 while [ "$1" != "" ]; do
@@ -40,11 +57,13 @@ while [ "$1" != "" ]; do
     shift
 done
 
+# CPPCHECK_LEVEL must be specified
 if [ -z "${CPPCHECK_LEVEL+x}" ]; then
     print_halp
     exit 1
 fi
 
+# If no source files or paths were specified, set it to the current directory
 if [ "${SOURCES}" == "" ]; then
     SOURCES="."
 fi
@@ -56,12 +75,11 @@ echo "SOURCES => ${SOURCES}"
 CPPCHECK="/usr/bin/env cppcheck"
 CPPCHECK_OPTIONS="--enable=${CPPCHECK_LEVEL} --force --library=c755a.cfg"
 
+# Build a string of -I (include path) for `cppcheck` to use
 unset INCLUDES
 for dir in `find ${PWD} -name "*.h*" -exec dirname {} \; | sort -u`; do
     INCLUDES="${INCLUDES} -I ${dir}"
 done
-
-#echo "INCLUDES => ${INCLUDES}"
 
 echo "Executing ${CPPCHECK} ${CPPCHECK_OPTIONS} ${OUTPUT_FORMAT} ${INCLUDES} ${SOURCES}"
 ${CPPCHECK} ${CPPCHECK_OPTIONS} ${OUTPUT_FORMAT} ${INCLUDES} ${SOURCES}
